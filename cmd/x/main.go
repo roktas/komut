1b7dea30915140899807d5cb31125ab48c572b66
Recai Oktaş <roktas@bil.omu.edu.tr>
2026-08-30 20:12:22 +0300

    feat: add hook transport mode

--- cmd/x/main.go
+++ cmd/x/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -9,21 +10,45 @@ import (
 )
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:], os.Stdin, os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "x: %v\n", err)
 		os.Exit(2)
 	}
 }
 
-func run() error {
-	input, err := readInvocation(os.Args[1:], os.Stdin)
+func run(args []string, stdin io.Reader, stdout io.Writer) error {
+	if len(args) == 1 && args[0] == "--hook" {
+		return runHook(stdin, stdout)
+	}
+
+	input, err := readInvocation(args, stdin)
 	if err != nil {
 		return err
 	}
+	return dispatch(input, "", stdout)
+}
 
-	cwd, err := os.Getwd()
-	if err != nil {
-		return fmt.Errorf("get working directory: %w", err)
+func runHook(stdin io.Reader, stdout io.Writer) error {
+	var input struct {
+		Prompt string `json:"prompt"`
+		CWD    string `json:"cwd"`
+	}
+	if err := json.NewDecoder(stdin).Decode(&input); err != nil {
+		return nil
+	}
+	if !komut.HasInvocationPrefix(input.Prompt) {
+		return nil
+	}
+	return dispatch(input.Prompt, input.CWD, stdout)
+}
+
+func dispatch(input, cwd string, stdout io.Writer) error {
+	var err error
+	if cwd == "" {
+		cwd, err = os.Getwd()
+		if err != nil {
+			return fmt.Errorf("get working directory: %w", err)
+		}
 	}
 	home, err := os.UserHomeDir()
 	if err != nil {
@@ -34,7 +59,7 @@ func run() error {
 	if err != nil {
 		return err
 	}
-	_, err = io.WriteString(os.Stdout, output)
+	_, err = io.WriteString(stdout, output)
 	return err
 }
 
